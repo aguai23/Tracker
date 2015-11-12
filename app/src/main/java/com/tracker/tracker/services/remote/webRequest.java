7637fbac98844b1bb9f75231f6f28b02458e546c
rmuthoo <rmuthoo@andrew.cmu.edu>
2015-11-11 23:10:02 -0500

    add stubs for implementation

---
+++ app/src/main/java/com/tracker/tracker/services/remote/webRequest.java
@@ -0,0 +1,11 @@
+package com.tracker.tracker.services.remote;
+
+/**
+ * Class to send request, and receive response
+ * from the remote service
+ */
+public class webRequest {
+    public String send_request(String payload){
+        return null;
+    }
+}
