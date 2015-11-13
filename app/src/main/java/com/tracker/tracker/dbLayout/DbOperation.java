7bc3de8908448bc73ee61d6b37343e7b4c2e6597
rmuthoo <rmuthoo@andrew.cmu.edu>
2015-11-12 19:52:16 -0500

    add dboperation

---
+++ app/src/main/java/com/tracker/tracker/dbLayout/DbOperation.java
@@ -0,0 +1,52 @@
+package com.tracker.tracker.dbLayout;
+
+import android.location.Location;
+import android.util.Pair;
+
+import com.tracker.tracker.model.PersonalInfo;
+import com.tracker.tracker.model.User;
+
+import java.security.Timestamp;
+import java.util.ArrayList;
+
+/**
+ * Database layer between Model and data source
+ */
+public class DbOperation {
+    public boolean create_user(PersonalInfo userInfo){
+
+        return false;
+    }
+
+    public boolean update_use(PersonalInfo userInfo){
+
+        return false;
+    }
+
+    public User login_user(String uname, String pwd){
+        User user = null;
+
+        return user;
+    }
+
+    public boolean approve_follower(PersonalInfo info){
+
+        return false;
+    }
+
+    public boolean remove_follower(PersonalInfo info){
+
+        return false;
+    }
+
+    public boolean send_request(PersonalInfo info){
+
+        return false;
+    }
+
+    public ArrayList<Pair<Location, Timestamp>> get_userLocation(PersonalInfo user){
+        ArrayList<Pair<Location, Timestamp>> locations = null;
+
+        return locations;
+    }
+}
