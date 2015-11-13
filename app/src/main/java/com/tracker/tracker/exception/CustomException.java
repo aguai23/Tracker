1c7442e5120bc3cefbe29d354ce828525932c187
Cody <yunzhezhang33@gmail.com>
2015-11-13 14:43:29 -0500

    create interface and abstract class

---
+++ app/src/main/java/com/tracker/tracker/exception/CustomException.java
@@ -0,0 +1,10 @@
+package com.tracker.tracker.exception;
+
+/**
+ * to deal with some kind of exception, will be implemented later
+ */
+public class CustomException extends  Exception{
+    public CustomException(){
+        super();
+    }
+}
