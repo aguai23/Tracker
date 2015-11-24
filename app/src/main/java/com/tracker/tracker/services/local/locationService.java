263d6ac7b71f0863cc063e5e566f2425de60ba5d
rmuthoo <rmuthoo@andrew.cmu.edu>
2015-11-24 17:50:50 -0500

    add dboperation

--- app/src/main/java/com/tracker/tracker/services/local/locationService.java
+++ app/src/main/java/com/tracker/tracker/services/local/locationService.java
@@ -1,16 +1,70 @@
 package com.tracker.tracker.services.local;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.tracker.tracker.services.remote.webRequest;
+
+import java.util.jar.Manifest;
 
 public class locationService extends Service {
+    private boolean STOP_MYSERVICE = false;
+
+    private long TIMEOUT = 10000;
+
+    private String TAG ="LOCATION_SERVICE";
+
+    private webRequest webservice;
+
     public locationService() {
+        webservice = new webRequest();
     }
 
     @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Nullable
+    @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        LocationManager locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+
+        synchronized (this) {
+            while (!STOP_MYSERVICE) {
+                try {
+                    wait(TIMEOUT);
+                    if (checkPermission(android.Manifest.permission.ACCESS_FINE_LOCATION, android.os.Process.myPid(), android.os.Process.myUid())
+                            != PackageManager.PERMISSION_GRANTED){
+                        Log.e(TAG, "No permissions fpr GPS");
+                    }
+                    Location locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+                    send_location(locationGPS.getLatitude(), locationGPS.getLongitude());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return START_STICKY;
+    }
+
+    private void send_location(double latitude, double longitude) {
+
     }
 }
--- app/src/main/java/com/tracker/tracker/services/remote/webRequest.java
+++ app/src/main/java/com/tracker/tracker/services/remote/webRequest.java
@@ -5,7 +5,29 @@ package com.tracker.tracker.services.remote;
  * from the remote service
  */
 public class webRequest {
+
+    enum REQUEST_TYPE{
+        SEND_LOCATION
+    }
+
+    private String createRequest(String data, REQUEST_TYPE type){
+        String request = null;
+
+        switch (type){
+            case SEND_LOCATION:
+                break;
+        }
+
+        return request;
+    }
+
     public String send_request(String payload){
         return null;
+    }
+
+    public void SendLocation(double lattitude, double longitude){
+        String request = createRequest(Double.toString(lattitude)+","+Double.toString(longitude), REQUEST_TYPE.SEND_LOCATION);
+
+        // send request to web service
     }
 }
